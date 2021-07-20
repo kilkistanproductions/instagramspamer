32cabf67e2e333550934fc7d91294f0ecec8b0fa
kilkistanproductions <gimcooldragon@gmail.com>
2021-07-20 19:45:20 +0300

    make it better

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import ui
 
 # setting up driver
-driver = webdriver.Firefox(executable_path=os.environ.get("GECKODRIVER_PATH"))
+FF_options = webdriver.FirefoxOptions()
+FF_profile = webdriver.FirefoxProfile()
+FF_options.add_argument("-headless")
+FF_profile.update_preferences()
+
+driver = webdriver.Firefox(options=FF_options, firefox_profile=FF_profile, executable_path=os.environ.get("GECKODRIVER_PATH"), firefox_binary=FirefoxBinary(os.environ.get("FIREFOX_BIN")))
 driver.get('https://www.instagram.com/')
 
 # log in
